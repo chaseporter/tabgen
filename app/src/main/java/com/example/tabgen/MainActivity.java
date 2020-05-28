9a8ae736416dd75ebf311032c6dabf061036a906
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-05-27 18:41:20 -0700

    Added databinding structure

--- app/src/main/java/com/example/tabgen/MainActivity.java
+++ app/src/main/java/com/example/tabgen/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.tabgen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.tabgen.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
