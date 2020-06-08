2972dc281d6aabd626b1e9c6e93f86a3d801a397
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-06-08 12:12:23 -0700

    Permission Handling
    Set OnClickListener for record button in MainActivity. Checks for
    permission to Record and requests if needed when button is clicked.

--- app/src/main/java/com/example/tabgen/MainActivity.java
+++ app/src/main/java/com/example/tabgen/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.tabgen;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.tabgen.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import javax.inject.Inject;
 
@@ -24,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
         binding.setState(appState);
         binding.setRecorder(recorder);
         super.onCreate(savedInstanceState);
+        final FloatingActionButton recordButton = findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (appState.isReady()) {
+                    if (checkPermissions())
+                        recorder.startRecording();
+                } else {
+                    recorder.stopRecording();
+                }
+            }
+        });;
+    }
+
+    boolean checkPermissions() {
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            return false;
+        }
+        return true;
     }
 }
